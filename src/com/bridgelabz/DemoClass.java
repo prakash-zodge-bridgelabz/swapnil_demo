bb9e039d913717e19c7bb49d510f913b1ea55533
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-04 11:24:03 +0530

    Added: Welcome by prakash

--- src/com/bridgelabz/DemoClass.java
+++ src/com/bridgelabz/DemoClass.java
@@ -2,6 +2,6 @@ package com.bridgelabz;
 
 public class DemoClass {
     public static void main(String[] args) {
-        System.out.println("Hello from sawpnil...");
+        System.out.println("Hello from prakash...");
     }
 }
